5c54155025a29a2132843254d7297f44d45d79f8
Matheus <matheusbelchior23@gmail.com>
2023-04-24 22:26:49 -0300

    alteração dinâmica dos campos de endereço a partir do CEP + verificação de erros

--- Projeto_5.Alura_Books/script.js
+++ Projeto_5.Alura_Books/script.js
@@ -1,13 +1,37 @@
 async function buscaEndereco(cep) {
+   var mensagemErro = document.getElementById('erro');
+   mensagemErro.innerHTML = "";
+   var cidade = document.getElementById('cidade');
+   var logradouro = document.getElementById('endereco');
+   var estado = document.getElementById('estado');
+   var bairro = document.getElementById('bairro');
    try {
       var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
       var consultaCEPJson = await consultaCEP.json();
+
       if (consultaCEPJson.erro) {
-         throw Error('CEP não existente!')
+         throw Error('CEP não existente!');
       };
-      console.log(consultaCEPJson);
+
+      cidade.value = consultaCEPJson.localidade;
+      logradouro.value = consultaCEPJson.logradouro;
+      estado.value = consultaCEPJson.uf;
+      bairro.value = consultaCEPJson.bairro;
       return consultaCEPJson;
    } catch (erro) {
+      if (erro.name == 'TypeError') {
+         mensagemErro.innerHTML = "<p>CEP inválido. Dígite seu CEP com 8 dígitos e sem símbolos ou pontos!</p>"
+      } else {
+         mensagemErro.innerHTML = "<p>CEP não encontrado. Tente novamente!</p>"
+      }
+      cidade.value = "";
+      logradouro.value = "";
+      estado.value = "";    
+      bairro.value = "";
       console.log(erro);
    };
 };
+
+var cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
+
