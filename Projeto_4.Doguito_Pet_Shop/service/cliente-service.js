7232411520c480e4aa753015633085d3847bcdf7
Matheus <matheusbelchior23@gmail.com>
2023-04-26 22:37:42 -0300

    alterando requisição dos dados de listaClientes para método fetch

--- Projeto_4.Doguito_Pet_Shop/service/cliente-service.js
+++ Projeto_4.Doguito_Pet_Shop/service/cliente-service.js
@@ -21,31 +21,12 @@ const criaNovaLinha = (nome, email) => {
 // Pegando a área html de lista_clientes onde serão inseridos os dados:
 const tabela = document.querySelector('[data-tabela]');
 
-// Adicionando na tabela uma nova linha para cada cliente a partir dos dados do BD (nome e email): 
-   //tabela.appendChild(criaNovaLinha(nome, email));
-// Vou inserir isso dentro do http.onload, para que ocorra para cada cliente cadastro no BD!
-
+//Usando a fetch, ela já faz o get e gera uma promisse automáticamente, then podemos retornar a resposta em formato json:
 const listaClientes = () => {
-   // Declarando uma promessa de requisição:
-   const promise = new Promise((resolve, reject) => {
-      // Inicializando objeto http de requisição:
-      const http = new XMLHttpRequest();
-
-      // Abrindo comunicação com o servidor, usando o método GET e endereço local para simular uma API!
-      http.open('GET', 'http://localhost:3000/profile');
-
-      // Ao carregar a página, se o status da solicitação for maior que 400 (erros cliente-servidor), rejeitaremos a promisse. Caso contrário, tudo certo. Lembrando de que em ambos os cenários temos que converter a resposta em objeto json:
-      http.onload = () => {
-         if(http.status >= 400) {
-            reject(JSON.parse(http.response))
-         } else {
-            resolve(JSON.parse(http.response))
-         }
-      };
-      http.send();
+   return fetch(`http://localhost:3000/profile`)
+   .then(resposta => {
+      return resposta.json(); 
    });
-   console.log(promise);
-   return promise;
 };
 
 // Ao carregar a página, teremos a comunicação com a API e os dados ficaram salvos em formato de texto (reponse). Convertemos isso em objetos JS, todos em um array. Para cada elemento do array vamos invocar a função de criar uma nova linha e já incluir essa linha na tabela HTML:
