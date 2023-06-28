2aae550d9ccb17b299991a25edcf2ef60bed07bf
Matheus <matheusbelchior23@gmail.com>
2023-06-28 12:43:48 -0300

    incluindo tela notFound

--- Projeto_7.Aluroni/src/routes.js
+++ Projeto_7.Aluroni/src/routes.js
@@ -4,10 +4,12 @@ import Menu from 'pages/Menu';
 import MenuLogo from 'components/MenuLogo';
 import PatternPage from 'components/PatternPage';
 import About from 'pages/About';
+import Footer from 'components/Footer';
+import NotFound from 'pages/NotFound';
 
 export default function appRouter() {
   return (
-    <main>
+    <main className='container'>
       <Router>
         <MenuLogo />
 
@@ -17,9 +19,11 @@ export default function appRouter() {
             <Route index element={<Init />} />
             <Route path="/menu" element={<Menu />} />
             <Route path="/about" element={<About />} />
+            <Route path='*' element={<NotFound />}/>
           </Route>
 
         </Routes>
+        <Footer />
 
       </Router>
     </main>
