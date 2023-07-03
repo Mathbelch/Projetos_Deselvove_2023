88f941e29923cbe4a8c2b776678a1a981d921c79
Matheus <matheusbelchior23@gmail.com>
2023-07-02 23:16:59 -0300

    finalizando validações

--- Projeto_5.Alura_Books/README.md
+++ Projeto_5.Alura_Books/README.md
@@ -1,4 +1,4 @@
-# Projeto Formulário Alura Bookd
+# Projeto Formulário Alura Books
 
 ## Objetivo: 
 Aplicar validação de formulário na página de cadastro da empresa fícticia Alura Books, de forma a se garantir a obtenção de dados padronizados para posterior inclusão no Banco de Dados. As validações foram feitas tanto no html para não permitir a submissão de dados fora do padrão, quanto em script.js para exibir mensagens de erro ao usuário em tempo real. Além disso ultilizou-se conceitos JS (Regex, fetch, assync/await, try/catch) para validações e comunicações com diferentes APIs (Brasil API, Who is - Email Verification API,  );
@@ -6,10 +6,10 @@ Aplicar validação de formulário na página de cadastro da empresa fícticia A
 ## Requisitos do Projeto:
 
 <ul>
-   <li>Todos os campos do formulário são de preenchimento obrigatório, exceto o 'complemento' em enderesso;</li>
+   <li>Todos os campos do formulário são de preenchimento obrigatório, exceto o 'complemento' em endereço;</li>
    <li>O campo de nome completo deve contér ao menos duas palavras (nome e sobrenome), e apenas usuários maiores de idade podem se inscrever;</li>
    <li>O usuário deve fornecer telefone de contato e e-mail válidos;</li>
-   <li>;</li>
+   <li>O CPF do usuário deve ser válidado com relação ao dígito verificador, mas outros erros podem ser informados ao usuário para melhorar a usabilidade;</li>
    <li>;</li>
 </ul>
 
@@ -24,12 +24,14 @@ Aplicar validação de formulário na página de cadastro da empresa fícticia A
 
 ## Validações:
    <ul>
-      <li>Nome: validação com Regex, checando se o nome possui ao menos duas palavras (nome - espaço - sobrenome), tanto no pattern do HTML input (para não permitir envio fora do padrão) quanto em js para exibir mensagem de erro;</li>
-      <li>Data de Nacimento: Realizada da mesma forma que para o nome, de forma a permtir o cadastro apenas de maiores de 18 anos;</li>
-      <li>Telefone de contato: deve possuir formato válido (DDD + 8 ou 9 dígitos), sendo o DDD válidado e seu estado identificado por consulta GET com a <a href="https://brasilapi.com.br/" alt="_blank">Brasil API</a>;</li>
-      <li>E-mail: A validação do email foi feita pela própria tag HTML e pela consulta na API <a href="https://emailverification.whoisxmlapi.com/api" alt="_blank"> <i>Who is - Email Verification</i></a>, checando tanto pela sintaxe quanto pelo dominío do e-mail informado;</li>
-      <li>CPF: npm i check-cpf;</li>
-      <li>Endereço: ;</li>
+      <li><b>Nome:</b> validação com Regex, checando se o nome possui ao menos duas palavras (nome - espaço - sobrenome), tanto no pattern do HTML input (para não permitir envio fora do padrão) quanto em js para exibir mensagem de erro;</li>
+      <li><b>Data de Nacimento:</b> Realizada da mesma forma que para o nome, de forma a permtir o cadastro apenas de maiores de 18 anos;</li>
+      <li><b>Telefone de contato:</b> deve possuir formato válido (DDD + 8 ou 9 dígitos), sendo o DDD válidado e seu estado identificado por consulta GET com a <a href="https://brasilapi.com.br/" alt="_blank">Brasil API</a>;</li>
+      <li><b>E-mail:</b> A validação do email foi feita pela própria tag HTML e pela consulta na API <a href="https://emailverification.whoisxmlapi.com/api" alt="_blank"> <i>Who is - Email Verification</i></a>, checando tanto pela sintaxe quanto pelo dominío do e-mail informado<sup>*</sup>;</li>
+      <small>*É necessário possuir cadastro no sistema e uma chave de acesso (apiKey), a qual deve ser incluida no arquivo keys.js</small>
+      <li><b>CPF:</b> Válidação realizada em código JS, verificando inicialmente se o valor informado possui 11 dígitos, se é formado apenas por números iguais (inválido) e, em seguida, são conferidos os dígitos verificadores<sup>*</sup>;</li>
+      <small>* Para o 1° digito verificador, somamos os 9 primeiros dígitos multiplicados de 10 até 2 (10 * 1°) + (9 * 2°) + (8 * 3°) + ... + (2 * 9°) e aplicamos a fórmula em confirma dígito. Para o 2° digito multipicador, somamos os 10 primeiros dígitos multiplicados de 11 até 2 (11 * 1°) + (10 * 2°) + (9 * 3°) + ... + (3 * 10°), e aplicamos na fórmula confirma dígito.</small>
+      <li><b>Endereço:</b> A validação do CEP foi feita pela pela consulta na API <a href="https://viacep.com.br/" alt="_blank"> <i>Via CEP</i></a>, já buscando e preenchendo as informações dos demais campos de endereço (exceto número e complemento);</li>
    </ul>
 
 
--- Projeto_5.Alura_Books/script.js
+++ Projeto_5.Alura_Books/script.js
@@ -39,8 +39,9 @@ async function validaTelefone(telefone) {
    // Deixando apenas números:
    telefone = telefone.replace(/\D/g, '');
 
-   // verificando se tem a qtde de numero correto:
-   if (!(telefone.length >= 10 && telefone.length <= 11)) {
+   if (telefone === "") {
+      mensagemErro.innerHTML = "É necessário informar seu número de contato";
+   } else if (!(telefone.length >= 10 && telefone.length <= 11)) {
       mensagemErro.innerHTML = "<p>Número de telefone inválido: dígito o número entre 10 (residêncial) e 11 (celular) dígitos, incluindo o DDD.</p>";
       return;
    };
@@ -123,9 +124,49 @@ function validaCPF(CPF) {
    // Puxando elementos HTML:
    var mensagemErro = document.getElementById('erroCPF');
    mensagemErro.innerHTML = "";
-   console.log(checkCpf(CPF))
+   const cpfFormatado = CPF.replace(/\D/g, '');
+   var regex = new RegExp('[0]{11}|[1]{11}|[2]{11}|[3]{11}|[4]{11}|[5]{11}|[6]{11}|[7]{11}|[8]{11}|[9]{11}');
+   const multiplicador = 10;
+   if (cpfFormatado === "") {
+      mensagemErro.innerHTML = "CPF deve ser preenchido";
+   } else if (cpfFormatado.length !== 11) {
+      mensagemErro.innerHTML = "Ops, parece que o CPF informado não possui 11 dígitos numéricos. Verifique e tente novamente.";
+   } else if (regex.test(cpfFormatado)) {
+      mensagemErro.innerHTML = "<p>O CPF não deve ter seus dígitos iguais. Revise e tente novamente</p>";
+   } else if (!checaDigitoVerificador(cpfFormatado, multiplicador)) {
+      mensagemErro.innerHTML = "<p>CPF inválido. Tente novamente.</p>"
+   };
 };
-// Vamos chamar a função assíncrona de comunicação com a API quando o campo de contato no HTML for preenchido e for clicado fora desse campo:
+
+// Calculo matemático: para o 1° digito verificador, somar os 9 primeiros dígitos multiplicados de 10 até 2 (10 * 1°) + (9 * 2°) + (8 * 3°) + ... + (2 * 9°) e aplicar a fórmula em confirma dígito. Para o 2° digito multipicador, somar os 10 primeiros dígitos multiplicados de 11 até 2 (11 * 1°) + (10 * 2°) + (9 * 3°) + ... + (3 * 10°), e aplicar na fórmula confirma dígito:
+function checaDigitoVerificador(cpf, multiplicador) {
+	if(multiplicador >= 12) {
+		return true;
+	};
+	let multiplicadorInicial = multiplicador;
+	let soma = 0;
+	const cpfSemDigitos = cpf.substr (0, multiplicador - 1).split('');
+	const digitoVerificador = cpf.charAt(multiplicador - 1);
+
+	for(let contador = 0; multiplicadorInicial > 1; multiplicadorInicial--) {
+		soma = soma + cpfSemDigitos[contador] * multiplicadorInicial;
+		contador++;
+	}
+
+	// Se o 1° dígito estiver correto, chamamos recursivamente a função para o 2° dígito verificador:
+	if(digitoVerificador == confirmaDigito(soma)) {
+		return checaDigitoVerificador(cpf, multiplicador + 1);
+	};
+
+	return false;
+
+};
+
+function confirmaDigito(soma) {
+	return (11 - (soma % 11));
+};
+
+// Vamos chamar a função assíncrona de comunicação com a API quando o campo de cpf no HTML for preenchido e for clicado fora desse campo:
 var cpf = document.getElementById('cpf');
 cpf.addEventListener("focusout", () => validaCPF(cpf.value));
 
@@ -139,6 +180,14 @@ async function buscaEndereco(cep) {
    var logradouro = document.getElementById('endereco');
    var estado = document.getElementById('estado');
    var bairro = document.getElementById('bairro');
+
+   if(cep === "") {
+      mensagemErro.innerHTML = "<p>Informe seu CEP para continuar.</p>"
+      return;
+   }
+
+   cep = cep.replace(/\D/g, '');
+
    // Comunicação com a API, conversando da resposta para Json e preenchimento dinâmico dos elementos HTML:
    try {
       var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
@@ -157,7 +206,7 @@ async function buscaEndereco(cep) {
    // Caso a Promisse seja rejeitada, podemos ter um Erro por digitação ou então um CEP não encontrado:
    } catch (erro) {
       if (erro.name == 'TypeError') {
-         mensagemErro.innerHTML = "<p>CEP inválido. Dígite seu CEP com 8 dígitos e sem símbolos ou pontos!</p>"
+         mensagemErro.innerHTML = "<p>CEP inválido. Dígite seu CEP com 8 dígitos!</p>"
       } else {
          mensagemErro.innerHTML = "<p>CEP não encontrado. Tente novamente!</p>"
       }
@@ -169,7 +218,6 @@ async function buscaEndereco(cep) {
       console.log(erro);
    };
 };
-
 // Vamos chamar a função assíncrona de comunicação com a API 
 var cep = document.getElementById('cep');
 cep.addEventListener("focusout", () => buscaEndereco(cep.value));
