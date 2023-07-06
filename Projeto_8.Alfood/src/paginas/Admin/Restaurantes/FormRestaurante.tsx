27a3c3dde772bc59ef55b940d3775507773bf20f
Matheus <matheusbelchior23@gmail.com>
2023-07-06 00:05:50 -0300

    incluindo métodos put e delete para podermos alterar o nome de restaurantes já cadastrados e deletar restaurantes.

--- Projeto_8.Alfood/src/paginas/Admin/Restaurantes/FormRestaurante.tsx
+++ Projeto_8.Alfood/src/paginas/Admin/Restaurantes/FormRestaurante.tsx
@@ -1,32 +1,56 @@
 import { Button, TextField } from "@mui/material";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import IRestaurante from "../../../interfaces/IRestaurante";
 
 
 const FormRestaurante = () => {
 
+   const parametros = useParams();
+   useEffect(() => {
+      if (parametros.id) {
+         axios.get<IRestaurante>(`http://localhost:8000/api/v2/restaurantes/${parametros.id}/`)
+            .then(resposta => setNomeRestaurante(resposta.data.nome))
+      }
+   }, [parametros]);
+
    const [nomeRestaurante, setNomeRestaurante] = useState('');
-   
+
 
    const onSubmitForm = (event: React.FormEvent<HTMLFormElement>) => {
       event.preventDefault();
-      axios.post('http://localhost:8000/api/v2/restaurantes/', {
-         nome: nomeRestaurante
-      })
+      if (parametros.id) {
+         axios.put(`http://localhost:8000/api/v2/restaurantes/${parametros.id}/`, {
+            nome: nomeRestaurante
+         })
+         .then(() => {
+            alert('Restaurante atualizado com sucesso')
+         })
+      } else {
+         axios.post('http://localhost:8000/api/v2/restaurantes/', {
+            nome: nomeRestaurante
+         })
          .then(() => {
             alert('Restaurante cadastrado com sucesso')
          })
+      }
+
+      
+      
+         
+      
    }
 
-   return(
-      <form onSubmit={ onSubmitForm }>
-         <TextField value={nomeRestaurante}
-          onChange={event => setNomeRestaurante(event.target.value)}
-          label="Nome do Restaurante" 
-          variant="standard" />
-         <Button type="submit" variant="outlined">Salvar</Button>
-      </form>
-   )
+return (
+   <form onSubmit={onSubmitForm}>
+      <TextField value={nomeRestaurante}
+         onChange={event => setNomeRestaurante(event.target.value)}
+         label="Nome do Restaurante"
+         variant="standard" />
+      <Button type="submit" variant="outlined">Salvar</Button>
+   </form>
+)
 }
 
 export default FormRestaurante;
--- Projeto_8.Alfood/src/paginas/Admin/Restaurantes/adminRestaurantes.tsx
+++ Projeto_8.Alfood/src/paginas/Admin/Restaurantes/adminRestaurantes.tsx
@@ -1,16 +1,25 @@
-import { TableContainer, Paper, Table, TableHead, TableRow, TableCell, TableBody } from "@mui/material";
+import { TableContainer, Paper, Table, TableHead, TableRow, TableCell, TableBody, Button } from "@mui/material";
 import IRestaurante from "../../../interfaces/IRestaurante";
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Link } from "react-router-dom";
 
 const AdminRestaurantes = () => {
 
    const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
 
    useEffect(() => {
-      axios.get('http://localhost:8000/api/v2/restaurantes/')
+      axios.get<IRestaurante[]>('http://localhost:8000/api/v2/restaurantes/')
          .then(resposta => setRestaurantes(resposta.data));
-   })
+   }, [])
+
+   const excluir = (restauranteExcluir: IRestaurante) => {
+      axios.delete(`http://localhost:8000/api/v2/restaurantes/${restauranteExcluir.id}/`)
+         .then(() => {
+            const listaRestaurante = restaurantes.filter(restaurante => restaurante.id !== restauranteExcluir.id)
+            setRestaurantes([...listaRestaurante])
+         })
+   }
 
    return (
       <TableContainer component={Paper}>
@@ -20,12 +29,26 @@ const AdminRestaurantes = () => {
                   <TableCell>
                      Nome
                   </TableCell>
+                  <TableCell>
+                     Editar
+                  </TableCell>
+                  <TableCell>
+                     Excluir
+                  </TableCell>
                </TableRow>
             </TableHead>
             <TableBody>
                {restaurantes.map(restaurante => <TableRow key={restaurante.id}>
                   <TableCell>
                      {restaurante.nome}
+                  </TableCell>
+                  <TableCell>
+                     [<Link to={`/admin/restaurantes/${restaurante.id}`}>editar</Link>]
+                  </TableCell>
+                  <TableCell>
+                     <Button variant="outlined" color="error" onClick={() => excluir(restaurante)}>
+                        Excluir
+                     </Button>
                   </TableCell>
                </TableRow>)}
             </TableBody>
