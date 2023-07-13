e2f06d6f630d398ada321da3a9d4927ec2b7fbb9
Matheus <matheusbelchior23@gmail.com>
2023-07-13 09:55:23 -0300

    inserindo hook useListaDeEventos, responsável por retornar a lista de eventos que está dentro do átomo

---
+++ Projeto_9.Event_Tracker/src/state/hooks/useListaDeEventos.ts
@@ -0,0 +1,9 @@
+import { listaDeEventosState } from "../atom";
+import { useRecoilValue } from 'recoil';
+
+
+const useListaDeEventos = () => {
+   return useRecoilValue(listaDeEventosState);
+}
+
+export default useListaDeEventos;
