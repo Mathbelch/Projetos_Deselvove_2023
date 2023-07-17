5244f54890c3f4180948a996ff27e77f1a1a71d9
Matheus <matheusbelchior23@gmail.com>
2023-07-17 12:20:55 -0300

    implementando funcionalidade de Debug e ajustando bug no Drag and Drop do calendário

--- Projeto_9.Event_Tracker/src/state/atom.ts
+++ Projeto_9.Event_Tracker/src/state/atom.ts
@@ -1,23 +1,11 @@
 import { atom } from 'recoil';
 import { IEvento } from '../interfaces/IEvento';
 import { IFiltro } from '../interfaces/IFiltro';
+import { eventosAsync } from './seletores';
 
 export const listaDeEventosState = atom<IEvento[]>({
    key: 'listaDeEventosState',
-   default: [{
-      "descricao": "Estudar React",
-      "inicio": new Date("2023-07-12T09:00"),
-      "fim": new Date("2023-07-12T13:00"),
-      "completo": false,
-      "id": 1642342747
-   },
-   {
-      "descricao": "Estudar Recoil",
-      "inicio": new Date("2023-07-13T09:00"),
-      "fim": new Date("2023-07-13T11:00"),
-      "completo": false,
-      "id": 1642342959
-   }]
+   default: eventosAsync
 })
 
 export const filtroDeEventos = atom<IFiltro>({
